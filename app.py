f68a44277b69532f539f0611130d12c58fae686c
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-01-25 22:33:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,37 +21,31 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    
-    news_title = request.form['news_title']
-    author_name = request.form['author_name']
-    text = request.form['text']
-    statement = news_title + " " + author_name + " " + text
-    
-    loaded_model = pickle.load(open("model.pickle.dat", "rb"))
-    wordnet=WordNetLemmatizer()
-    review = re.sub('[^a-zA-Z]', ' ', statement)
-    review= review.lower()
-    review= review.split()
-    review = [wordnet.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
-    review= ' '.join(review)
-
-    corpus_test = []    
-    corpus_test.append(review)
-    
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    cv = TfidfVectorizer()
-    X = cv.fit_transform(corpus_test).toarray()
-    
-    length = 5000 - len(X[0])
-    X = np.pad(X, (0, length), mode = 'mean')
-    
-    X_final = [[round(value) for value in X[0]]]
-    X_final = np.array(X_final)
-    
-    pred = loaded_model.predict(X_final)
-    print(pred)
-    dict = {1: 'real', 0: 'fake'}
-    prd = ('Your prediction is: ',dict[pred[0]])
+    if request.method == 'POST':
+        news_title = request.form['news_title']
+        author_name = request.form['author_name']
+        text = request.form['text']
+        statement = news_title + " " + author_name + " " + text
+        loaded_model = pickle.load(open("model.pickle.dat", "rb"))
+        wordnet=WordNetLemmatizer()
+        review = re.sub('[^a-zA-Z]', ' ', statement)
+        review= review.lower()
+        review= review.split()
+        review = [wordnet.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
+        review= ' '.join(review)
+        
+        corpus_test = []    
+        corpus_test.append(review)
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        cv = TfidfVectorizer()
+        X = cv.fit_transform(corpus_test).toarray()
+        length = 5000 - len(X[0])
+        X = np.pad(X, (0, length), mode = 'mean')
+        X_final = [[round(value) for value in X[0]]]
+        X_final = np.array(X_final)
+        pred = loaded_model.predict(X_final)
+        dict = {1: 'real', 0: 'fake'}
+        prd = ('Your prediction is: ',dict[pred[0]])
 
     return render_template('index.html', prediction_text=prd)
 
