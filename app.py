2abef19dc5334de40a728ebafbd9cd31b499eda6
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-01-25 21:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,14 @@
 from flask import Flask, request, jsonify, render_template
 
 
-from feature import *
 
+import pickle
+import numpy as np
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 app = Flask(__name__)
 
@@ -20,7 +26,32 @@ def predict():
     author_name = request.form['author_name']
     text = request.form['text']
     statement = news_title + " " + author_name + " " + text
-    prd = fakenews(statement)
+    
+    loaded_model = pickle.load(open("model.pickle.dat", "rb"))
+    wordnet=WordNetLemmatizer()
+    review = re.sub('[^a-zA-Z]', ' ', statement)
+    review= review.lower()
+    review= review.split()
+    review = [wordnet.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
+    review= ' '.join(review)
+
+    corpus_test = []    
+    corpus_test.append(review)
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    cv = TfidfVectorizer()
+    X = cv.fit_transform(corpus_test).toarray()
+    
+    length = 5000 - len(X[0])
+    X = np.pad(X, (0, length), mode = 'mean')
+    
+    X_final = [[round(value) for value in X[0]]]
+    X_final = np.array(X_final)
+    
+    pred = loaded_model.predict(X_final)
+    print(pred)
+    dict = {1: 'real', 0: 'fake'}
+    prd = ('Your prediction is: ',dict[pred[0]])
 
     return render_template('index.html', prediction_text=prd)
 
