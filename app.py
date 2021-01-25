7a358603f0b0c72111bc9778f64dc75899be339c
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-01-25 10:26:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+
+from feature import *
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+
+    news_title = request.form['news_title']
+    author_name = request.form['author_name']
+    text = request.form['text']
+    statement = news_title + " " + author_name + " "+ text 
+    prd = fakenews(statement)
+    return f'<html><body><h1>{prd}</h1> <form action="/"> <button type="submit">back </button> </form></body></html>'
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
---
+++ feature.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+def fakenews(statement):
+    loaded_model = pickle.load(open("model.pickle.dat", "rb"))
+    wordnet=WordNetLemmatizer()
+    review = re.sub('[^a-zA-Z]', ' ', statement)
+    review= review.lower()
+    review= review.split()
+    review = [wordnet.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
+    review= ' '.join(review)
+
+    corpus_test = []    
+    corpus_test.append(review)
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    cv = TfidfVectorizer()
+    X = cv.fit_transform(corpus_test).toarray()
+    
+    length = 5000 - len(X[0])
+    X = np.pad(X, (0, length), mode = 'mean')
+    
+    X_final = [[round(value) for value in X[0]]]
+    X_final = np.array(X_final)
+    
+    pred = loaded_model.predict(X_final)
+    print(pred)
+    dict = {1: 'real', 0: 'fake'}
+    return ('Your prediction is: ',dict[pred[0]])
